3cfcf3855c13c505859a84b4aa8af7ff38f530cb
Crazy_Fox <holmyliszag@ukr.net>
2025-09-11 15:24:05 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const forma = document.querySelector('.login-form');
+
+forma.addEventListener('submit', event => {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const pass = form.elements.password.value.trim();
+
+  if (email === '' || pass === '') {
+    alert('All form fields must be filled in');
+  } else {
+    const user = {
+      email: email,
+      password: pass,
+    };
+    form.reset();
+    console.log(user);
+  }
+});
