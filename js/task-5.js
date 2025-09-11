aa63845038f033cdb7e5da1030ea59f901c83eed
Crazy_Fox <holmyliszag@ukr.net>
2025-09-11 15:59:45 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -5,3 +5,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const span = document.querySelector('.color');
+
+btn.addEventListener('click', colorChange);
+
+function colorChange() {
+  const hex = getRandomHexColor();
+  body.style.backgroundColor = hex;
+  span.style.backgroundColor = hex;
+  span.textContent = hex;
+}
